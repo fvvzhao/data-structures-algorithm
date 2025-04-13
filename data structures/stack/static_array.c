8669a4717205fbed2415e266bea298a2e5882dce
fvvzhao <fvvzhao@gmail.com>
2025-04-14 00:46:37 +0800

    Update static_array.c

--- data structures/stack/static_array.c
+++ data structures/stack/static_array.c
@@ -1,1 +1,55 @@
+#include <stdio.h>
 
+#define CAPACITY 10
+
+typedef struct Stack {
+    int data[CAPACITY];
+    int top;
+} Stack;
+
+void create(Stack* ptr);
+void push(Stack* ptr, int value);
+void pop(Stack* ptr, int* value);
+int get_top(Stack* ptr);
+
+int main(void) {
+
+    Stack test;
+    Stack* ptr = &test;
+    create(ptr);
+
+    push(ptr, 1);
+    printf("top is %d\n", get_top(ptr));
+
+    return 0; 
+}
+
+void create(Stack* ptr) {
+    ptr->top = -1;
+}
+
+void push(Stack* ptr, int value) {
+    if (ptr->top == CAPACITY - 1) {
+        puts("栈已满");
+        return;
+    }
+    ptr->top++;
+    ptr->data[ptr->top] = value;
+}
+
+void pop(Stack* ptr, int* value) {
+    if (ptr->top == -1) {
+        puts("栈已空");
+        return;
+    }
+    *value = ptr->data[ptr->top];
+    ptr->top--;
+}
+
+int get_top(Stack* ptr) {
+    if (ptr->top == -1) {
+        puts("栈已空");
+        return -1;
+    }
+    return ptr->data[ptr->top];
+}
