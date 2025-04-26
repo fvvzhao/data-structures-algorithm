ec84496af322d3456c6fefee4d02c6870eb31455
fvvzhao <fvvzhao@gmail.com>
2025-04-26 11:53:44 +0800

    Update static_array.c

--- data structures/queue/static_array.c
+++ data structures/queue/static_array.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define CAPACITY 10
 
@@ -9,57 +10,60 @@ typedef struct Queue {
     int tail;
 } Queue;
 
-void create(Queue* ptr);
-void en_queue(Queue* ptr, int value);
-void de_queue(Queue* ptr, int* value);
+Queue* create_queue();
+void en_queue(Queue* q, int value);
+int de_queue(Queue* q);
 void print_queue(Queue* ptr);
 
 int main(void) {
 
-    Queue test;
-    Queue* ptr = &test;
-    create(ptr);
+    Queue* q = create_queue();
 
-    en_queue(ptr, 1);
-    en_queue(ptr, 2);
-    en_queue(ptr, 3);
-    en_queue(ptr, 4);
-    en_queue(ptr, 5);
+    en_queue(q, 1);
+    en_queue(q, 2);
+    en_queue(q, 3);
+    en_queue(q, 4);
+    en_queue(q, 5);
 
-    print_queue(ptr);
+    print_queue(q);
     return 0;
 }
 
-void create(Queue* ptr) {
+Queue* create_queue() {
+    Queue* ptr = (Queue*)malloc(sizeof(Queue));
+    if (ptr == NULL) {
+        puts("内存分配失败"); 
+        return NULL;
+    }
     ptr->size = 0;
     ptr->head = 0;
     ptr->tail = 0;
+    return ptr;
 }
 
-void en_queue(Queue* ptr, int value) {
-    if (ptr->size == CAPACITY) {
+void en_queue(Queue* q, int value) {
+    if (q->size == CAPACITY) {
         puts("队列已满");
         return;
     }
-    ptr->data[ptr->tail] = value;
-    ptr->tail = (ptr->tail + 1) % CAPACITY;
-    ptr->size++;
+    q->data[q->tail] = value;
+    q->tail = (q->tail + 1) % CAPACITY;
+    q->size++;
 }
 
-void de_queue(Queue* ptr, int* value) {
-    if (ptr->size == 0) {
+int de_queue(Queue* q) {
+    if (q->size == 0) {
         puts("队列已空");
-        return;
+        return -1;
     }
-    *value = ptr->data[ptr->head];
-    ptr->head = (ptr->head + 1) % CAPACITY;
-    ptr->size--;
+    int t = q->data[q->head];
+    q->head = (q->head + 1) % CAPACITY;
+    q->size--;
+    return t;
 }
 
 void print_queue(Queue* ptr) {
-    int value;
-    while (ptr->head != ptr->tail) {
-        de_queue(ptr, &value);
-        printf("%d ", value);
+    for (int i = ptr->head; i != ptr->tail; i = (i + 1) % CAPACITY) {
+        printf("%d ", ptr->data[i]);
     }
 }
