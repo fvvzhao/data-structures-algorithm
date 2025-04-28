5748055342f19d11ec610745f83f68ab6395f65c
fvvzhao <fvvzhao@gmail.com>
2025-04-28 21:12:28 +0800

    Update linear_search.c

--- algorithm/search/linear_search.c
+++ algorithm/search/linear_search.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+int linear_search(int nums[], int size, int target);
+
+int main(void) {
+
+    int nums[] = {6, 7, 8, 9, 10};
+    int size = sizeof(nums) / sizeof(int);
+    printf("%d", linear_search(nums, size, 8));
+    
+    return 0;
+}
+
+int linear_search(int nums[], int size, int target) {
+    for (int i = 0; i < size; i++)
+    {
+        if (nums[i] == target) {
+            return i;
+        }
+    }
+    return -1;
+}
